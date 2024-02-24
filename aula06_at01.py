5609a905db51d75781e2d220317e687aea5a76d5
PAULO HENRIQUE DOS SANTOS <paulo.hsantos26@senacsp.edu.br>
2024-02-24 09:24:26 -0300

    Class

---
+++ aula06_at01.py
@@ -0,0 +1,13 @@
+from aula06_at01Class import recibo
+
+
+dados = recibo('Paulo Santos')
+
+# print(dados.valor)
+
+dados.valor = 52.2567
+dados.descricao('Aulinha de Python-zin')
+
+# print(dados.valor)
+
+print(dados)
---
+++ aula06_at01Class.py
@@ -0,0 +1,24 @@
+class recibo:
+    def __init__(self, nome):
+        self._nome = nome
+        self._valor = 0
+        self._descricao = 'Sistema genérico de qualidade'
+        #self._nome = nome -> variavel publica
+        #self.__nome = nome -> variavel privada
+    
+    def __str__(self):
+        texto = 'Recebemos de {} a quantia de R${:.2f}'.format(self._nome, self._valor)
+        descricao = 'Referente {}'.format(self._descricao)
+        dados = '{}\n{}'.format(descricao, texto)
+        return dados 
+
+    @property
+    def valor(self):
+        return self._valor
+    
+    @valor.setter
+    def valor(self, value):
+        self._valor = value
+    
+    def descricao(self, value):
+        self._descricao = value
