af8d764c98a332a6c92b6d112826ab9cd3ff08e5
Paulo Not <paulo.whsantos@hotmail.com>
2024-02-27 14:57:19 -0300

    atividade 04 completa

--- aula06_at04.py
+++ aula06_at04.py
@@ -1,10 +1,33 @@
-caminho_arquivo = 'categorias.csv'
-categorias = open(caminho_arquivo, 'r', encoding='utf-8')
+from os import system
+import openpyxl
 
-lista_categoria = [] #-> chaves
+def CarregarArquivoCSV(nome_arquivo):
+    caminho_arquivo = nome_arquivo + '.csv'
+    categorias = open(caminho_arquivo, 'r', encoding='utf-8')
 
-for linha in categorias:
-    colunas = linha.strip().split(';')
-    lista_categoria.append([colunas[2] == colunas[0]])
+    lista_categoria = [] # -> com a abertura do cochetes temos um array vazio
 
-print(lista_categoria)
+    for linha in categorias:
+        colunas = linha.strip().split(';')
+        lista_categoria.append(colunas)
+
+    return lista_categoria
+
+def GravarArquivoXLSX(dados, nome_arquivo):
+    try:
+        excel = openpyxl.Workbook() # -> criando o excel
+        planilha = excel.active # -> a primeira aba ativa
+
+        for linha in dados:
+            planilha.append(linha) # -> adicionar mais infomações
+        excel.save(nome_arquivo + '.xlsx') # -> fecho a conexão e dou o nome para o arquivo
+
+        print('Dados salvo com sucesso no arquivo {}'.format(nome_arquivo))
+
+    except Exception as ex:
+        print('Ocorreu um erro {}'.format(ex))
+
+system('cls')
+nome_arquivo = input('Digite o nome do arquivo: (sem as extensão) ')
+dadosArquivoCSV = CarregarArquivoCSV(nome_arquivo) # -> consultamos o arquivo
+GravarArquivoXLSX(dadosArquivoCSV, nome_arquivo) # -> passamos o resultado da ultima consulta e gravamos no novo tipo de arquiv
